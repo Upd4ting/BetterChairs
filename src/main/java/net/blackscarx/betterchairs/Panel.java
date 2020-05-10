0d27e267bed28bdb2500581fbafe1a227106925f
Christian Koop <developer@sprax2013.de>
2020-05-10 12:54:38 +0200

    Recode Panel.java
    Problems fixed now:
    * Running it on a server (or headless) threw an exception
    * Version string was outdated and had to be updated manually. Now parses plugin.yml
    * Running it did not show it on taskbar (Windows 10)

--- src/main/java/net/blackscarx/betterchairs/Panel.java
+++ src/main/java/net/blackscarx/betterchairs/Panel.java
@@ -1,21 +1,54 @@
 package net.blackscarx.betterchairs;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.awt.GraphicsEnvironment;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
-/**
- * Created by BlackScarx on 11/05/2016.
- * BlackScarx all right reserved
- */
-public class Panel extends JPanel {
-
-    public Panel() {
-    }
-
+public class Panel {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(new Panel(), "This is a plugin.\n" +
-                "Just place it in the plugins folder of your server\n" +
-                "BetterChairs Version: 0.10.1", "BetterChairs", JOptionPane.INFORMATION_MESSAGE);
-        System.exit(0);
-    }
+        String version = "UNKNOWN", web = null;
 
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(Panel.class.getResourceAsStream("/plugin.yml"), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null && (version.equals("UNKNOWN") || web == null)) {
+                if (line.startsWith("version:")) {
+                    version = line.substring(8).trim();
+                } else if (line.startsWith("website:")) {
+                    web = line.substring(8).trim();
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder("This is a Minecraft Spigot Plugin (SpigotMC.org).\n")
+                .append("Just place it inside your server's plugin folder.\n\n")
+                .append("Version: ")
+                .append(version).append("\n");
+
+        if (web != null) {
+            sb.append("Website: ")
+                    .append(web);
+        }
+
+        if (!GraphicsEnvironment.isHeadless()) {
+            // Create whole JFrame to make sure the window appears in the system's taskbar
+            JFrame frame = new JFrame();
+            frame.setUndecorated(true);
+            frame.setVisible(true);
+            frame.setLocationRelativeTo(null);
+            frame.setIconImage(new ImageIcon(Panel.class.getResource("/icon.png")).getImage());
+
+            JOptionPane.showMessageDialog(frame, sb.toString(), "BetterChairs v" + version, JOptionPane.INFORMATION_MESSAGE);
+            frame.dispose();
+        } else {
+            System.out.println(sb.toString());
+        }
+    }
 }
